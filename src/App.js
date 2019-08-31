91c566c832fc68b3ab2e9495a453d6c31b0ec53c
Radwa Sayed <rdw_syd@yahoo.com>
2019-08-31 23:18:22 +0200

    added checkout page, checkout item component and updated cart to use selectors from reselct

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { createStructuredSelector } from "reselect";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 import './App.css';
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
@@ -46,6 +47,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route exact path='/shop' component={ShopPage}/>
+                    <Route exact path='/checkout' component={CheckoutPage}/>
                     <Route
                         exact
                         path='/signin'
