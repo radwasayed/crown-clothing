1c81f50e3a44d5036050ca7b032aebfa36fdd4cd
Radwa Sayed <rdw_syd@yahoo.com>
2019-09-02 22:05:58 +0200

    created collection component, added nested url into our shop page and change our shop data array an to object

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import CollectionOverview from '../../components/collections-overview/collections-overview.component'
+import { Route } from 'react-router-dom';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from "../collection/collection.component";
 
 import './shop.styles.scss';
 
-
-const ShopPage  = () => (
+const ShopPage  = ({ match }) => (
     <div className="shop-page">
-        <CollectionOverview />
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
     </div>
 );
-
-
 
 export default ShopPage;
